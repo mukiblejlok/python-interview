98b85603785ef01ba2dc46c033d0414ca8130b18
Filip Mularczyk <filip.mularczyk@pl.abb.com>
2021-07-06 13:45:24 +0200

    add two bug questions

---
+++ questions/bug1.py
@@ -0,0 +1,47 @@
+"""
+Question
+
+Find the bug in generate_report implementation.
+
+When you run the __main__ you will see that the Reports 1, 2, 4 are same and somehow combined,
+while the Report 3 looks OK.
+Why it's so?
+How to fix it?
+
+"""
+from typing import List
+
+DEFAULT_TEMPLATE = ["First Page", "TOC"]
+
+
+def generate_report(results: int, template: List[str] = DEFAULT_TEMPLATE) -> List[str]:
+    # Start from provided template
+    report = template
+    # Add introduction
+    report.append("Introduction")
+    # Add analysis results
+    report.append(f"Analysis results: {results}")
+    # Add Summary
+    report.append("Summary")
+
+    return report
+
+
+if __name__ == '__main__':
+    # First report with default template
+    report1 = generate_report(results=1)
+
+    # Second report with default template
+    report2 = generate_report(results=2)
+
+    # Report with custom template
+    report3 = generate_report(results=3, template=["New First Page", "Slim - TOC"])
+
+    # Third report with default template
+    report4 = generate_report(results=4)
+
+    # Print results
+    print(report1)
+    print(report2)
+    print(report3)
+    print(report4)
---
+++ questions/bug2.py
@@ -0,0 +1,28 @@
+"""
+Question
+
+Find the bug in compare_ints implementation.
+
+When you run the __main__ you will see that it works well for some integers but not for all of them
+
+Why it's so?
+How to fix it?
+
+"""
+
+
+def compare_ints(in1: int, in2: int) -> bool:
+    if not isinstance(in1, int) or not isinstance(in2, int):
+        return False
+    if in1 is in2:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    valid_test_cases = [
+        (1, 1), (22, 22), (255, 255), (259, 259), (1000, 1000), (0, 0)
+    ]
+    for test_in1, test_in2 in valid_test_cases:
+        print(f"is {test_in1} equal to {test_in2}?, {compare_ints(test_in1, test_in2)}")
